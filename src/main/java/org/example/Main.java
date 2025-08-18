28ff42821b2704e6a604a4b22916eaca3862afd9
lara-conte-gomes <lara.lc@gec.inatel.br>
2025-08-18 19:50:15 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main{
+    public static void main(String[] args) {
+        Map<String, String> obj = new HashMap<>();
+        obj.put("message", "Hello world!");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(obj);
+
+        System.out.println(json);
+    }
+}
